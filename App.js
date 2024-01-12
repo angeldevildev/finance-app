660e6bab18b01e25769a306af1611bdde051dd67
mineshopdev <mineshop47@gmail.com>
2024-01-12 22:40:10 +0100

    File setup

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import React, { useState } from "react";
+import { useCallback } from "react";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "SFProText-Regular": require("./assets/fonts/.otf/SF-Pro-Text-Light.otf"),
+    "SFProText-Medium": require("./assets/fonts/.otf/SF-Pro-Text-Medium.otf"),
+    "SFProText-Regular": require("./assets/fonts/.otf/SF-Pro-Text-Regular.otf"),
+    "SFProText-Semibold": require("./assets/fonts/.otf/SF-Pro-Text-Semibold.otf")
+    // Aggiungi altre varianti 
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
